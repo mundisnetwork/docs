e7ad25ddd01b4ecd4661f071e1f7969ddb4b19e5
flaviusburca <flavius.burca@localhost>
2022-05-30 14:26:26 +0300

    Move from github pages to S3+CloudFront

---
+++ publish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm run build
+aws s3 rm s3://docs.mundis.io --recursive
+aws s3 cp build/ s3://docs.mundis.io --recursive
+aws cloudfront create-invalidation --distribution-id E1ZUYFJZ3JZC8N --paths "/*"
